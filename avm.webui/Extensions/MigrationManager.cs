8ddb8265cc444698ab09248840d1b6789625ae30
blacklight44 <yakupyakut@yandex.com>
2024-04-21 01:40:07 +0300

    migrationmanegerin olusturulması

---
+++ avm.webui/Extensions/MigrationManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using avm.data.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace avm.webui.Extensions
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                // using (var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>())
+                // {
+                //     try
+                //     {
+                //         applicationContext.Database.Migrate();
+                //     }
+                //     catch (System.Exception)
+                //     {
+                //         // loglama
+                //         throw;
+                //     }
+                // }
+
+                using (var shopContext = scope.ServiceProvider.GetRequiredService<ShopContext>())
+                {
+                    try
+                    {
+                        shopContext.Database.Migrate();
+                    }
+                    catch (System.Exception)
+                    {
+                        // loglama
+                        throw;
+                    }
+                }
+            }
+       
+            return host;
+        }
+    }
+}
--- avm.webui/Startup.cs
+++ avm.webui/Startup.cs
@@ -36,7 +36,7 @@ namespace avm.webui
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ShopContext>(options=> options.UseSqlServer(_configuration.GetConnectionString("MsSqlConnection")));
+            services.AddDbContext<ShopContext>(options=> options.UseMySql(_configuration.GetConnectionString("MySqlConnection")));
              
              services.AddControllersWithViews();
         }
