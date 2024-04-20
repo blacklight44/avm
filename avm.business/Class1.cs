406fe3d406879d70de569ab2a7c0c62f07daeed6
blacklight44 <yakupyakut@yandex.com>
2024-04-20 20:31:40 +0300

    dosyaların olusturulup mimarinin kurulmasi

---
+++ avm.business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace avm.business
+{
+    public class Class1
+    {
+    }
+}
---
+++ avm.data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace avm.data
+{
+    public class Class1
+    {
+    }
+}
